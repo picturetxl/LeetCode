27d503e4dd277d4e2a360c5de279b7a7b58f0530
picturetxl <txltech123@163.com>
2022-07-23 01:26:58 +0800

    01 two sum 暴力解法

---
+++ 01-two-sum.cpp
@@ -0,0 +1,27 @@
+#include "leetcode.h"
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> result;
+		for(size_t i = 0; i < nums.size();i++){
+			for (size_t j = i + 1; j < nums.size();j++){
+				if (nums[i] + nums[j] == target){
+					result.push_back(i);
+					result.push_back(j);
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+};
+
+
+
+int main(){
+
+
+
+}
