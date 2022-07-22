6975c6dd81982e4cfedc03e65c6f85fdcaef005d
picturetxl <txltech123@163.com>
2022-07-23 01:51:28 +0800

    01 two sum O(n)解法

--- 01-two-sum.cpp
+++ 01-two-sum.cpp
@@ -5,19 +5,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 		vector<int> result;
+		unordered_map<int,int> map;
 		for(size_t i = 0; i < nums.size();i++){
-			for (size_t j = i + 1; j < nums.size();j++){
-				if (nums[i] + nums[j] == target){
-					result.push_back(i);
-					result.push_back(j);
-					return result;
-				}
+			if (map.find(target-nums[i])!=map.end()){
+				result.push_back(map[target-nums[i]]);
+				result.push_back(i);
+				return result;
 			}
+			map[nums[i]]=i;
 		}
 		return result;
 	}
 };
-
 
 
 int main(){
--- leetcode.h
+++ leetcode.h
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <unordered_map>
 #include <string>
 #include <vector>
 
